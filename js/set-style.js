e021d29e588a24d7b51050bb4b3557b11d1a3c8b
Lina Lu <linalu001@gmail.com>
2017-04-18 17:47:34 -0700

    [layout] added new layout

--- js/set-style.js
+++ js/set-style.js
@@ -1,24 +1,34 @@
-var setVideoPosition = function() {
-	var video = document.getElementsByTagName("video")[0];
-	var width = window.innerWidth;
-	var height = window.innerHeight;
-	var widthHeightRatio = 1600/1200;
-	var vidWidth = height * widthHeightRatio;
-	var leftMargin = (width - vidWidth) / 2;
-	video.style.marginLeft = JSON.stringify(leftMargin) + "px";
-}
+// var setVideoPosition = function() {
+// 	var video = document.getElementsByTagName("video")[0];
+// 	var width = window.innerWidth;
+// 	var height = window.innerHeight;
+// 	var widthHeightRatio = 1600/1200;
+// 	var vidWidth = height * widthHeightRatio;
+// 	var leftMargin = (width - vidWidth) / 2;
+// 	video.style.marginLeft = JSON.stringify(leftMargin) + "px";
+// 	// setNavPosition(leftMargin);
+// }
 
-var setLogoPosition = function() {
-	var gamelogo = document.getElementsByClassName("game_logo_wrapper")[0];
-	var width = window.innerWidth;
-	var height = window.innerHeight;
-	var widthHeightRatio = 777/292;
-	var logoHeight = .1*height;
-	var logoWidth = logoHeight * widthHeightRatio;
-	var leftMargin = (width - logoWidth)/2;
-	gamelogo.style.marginLeft = JSON.stringify(leftMargin) + "px";
-	var topMargin = height * .01;
-	gamelogo.style.marginTop = JSON.stringify(topMargin) + "px";
-}
-setVideoPosition();
-setLogoPosition();
+// var setLogoPosition = function() {
+// 	var gamelogo = document.getElementsByClassName("game_logo_wrapper")[0];
+// 	var width = window.innerWidth;
+// 	var height = window.innerHeight;
+// 	var widthHeightRatio = 777/292;
+// 	var logoHeight = .1*height;
+// 	var logoWidth = logoHeight * widthHeightRatio;
+// 	var leftMargin = (width - logoWidth)/2;
+// 	gamelogo.style.marginLeft = JSON.stringify(leftMargin) + "px";
+// 	var topMargin = height * .01;
+// 	gamelogo.style.marginTop = JSON.stringify(topMargin) + "px";
+// }
+
+// var setNavPosition = function(leftMargin) {
+// 	var width = window.innerWidth;
+// 	var logoWidth = width * .12;
+
+// 	var nav = document.getElementsByClassName("website-nav")[0];
+// 	var rightMargin = (leftMargin - logoWidth)/2;
+// 	nav.style.marginRight = JSON.stringify(rightMargin) + "px";
+// }
+// // setVideoPosition();
+// // setLogoPosition();
