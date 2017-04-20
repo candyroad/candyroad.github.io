9edddfc960cdb713da14d2e48a20c2c9b25c7018
Lina Lu <linalu001@gmail.com>
2017-04-20 00:08:01 -0700

    [repsonsive] added basic responsive

--- js/set-style.js
+++ js/set-style.js
@@ -1,22 +1,48 @@
-var setVideoPosition = function() {
-	var video = document.getElementsByTagName("video")[0];
+// var setVideoPosition = function() {
+// 	var video = document.getElementsByTagName("video")[0];
+// 	var width = window.innerWidth;
+// 	var height = window.innerHeight;
+// 	var widthHeightRatio = 1600/1200;
+// 	// if(height/width > 1) {
+// 	// 	video.style.width = "100vw";
+// 	// 	video.style.height="auto";
+// 	// 	var vidHeight = width / widthHeightRatio;
+// 	// 	var topMargin = (height - vidHeight)/2;
+// 	// 	// video.style.marginTop=JSON.stringify(topMargin)+ "px";
+// 	// 	return;
+// 	// }
+// 	var vidWidth = height * widthHeightRatio * .83;
+// 	var leftMargin = (width - vidWidth) / 2;
+// 	video.style.marginLeft = JSON.stringify(leftMargin) + "px";
+// 	// setNavPosition(leftMargin);
+// }
+var setVideoPositionTest = function() {
 	var width = window.innerWidth;
 	var height = window.innerHeight;
-	var widthHeightRatio = 1600/1200;
-	// if(height/width > 1) {
-	// 	video.style.width = "100vw";
-	// 	video.style.height="auto";
-	// 	var vidHeight = width / widthHeightRatio;
-	// 	var topMargin = (height - vidHeight)/2;
-	// 	// video.style.marginTop=JSON.stringify(topMargin)+ "px";
-	// 	return;
-	// }
-	var vidWidth = height * widthHeightRatio * .83;
-	var leftMargin = (width - vidWidth) / 2;
-	video.style.marginLeft = JSON.stringify(leftMargin) + "px";
+	// var widthHeightRatio = 1600/1200;
+	if(height/width <= 1) {
+		return;
+		// video.style.height="auto";
+		// var vidHeight = width / widthHeightRatio;
+		// var topMargin = (height - vidHeight)/2;
+		// video.style.marginTop=JSON.stringify(topMargin)+ "px";
+		// return;
+	}
+	var video = document.getElementsByTagName("video")[0];
+	var videoWrapper = document.getElementsByClassName("html-videoplayer")[0];
+	video.style.width = "100vw";
+	video.style.height="auto";
+	// videoWrapper.style.marginTop = "1rem";
+	videoWrapper.style.display = "block";
+	videoWrapper.style.justifyContent="none";
+	// var vidWidth = height * widthHeightRatio * .83;
+	// var leftMargin = (width - vidWidth) / 2;
+	// video.style.marginLeft = JSON.stringify(leftMargin) + "px";
 	// setNavPosition(leftMargin);
 }
-
+window.onload = function() {
+	setVideoPositionTest();
+};
 // var setLogoPosition = function() {
 // 	var gamelogo = document.getElementsByClassName("game_logo_wrapper")[0];
 // 	var width = window.innerWidth;
